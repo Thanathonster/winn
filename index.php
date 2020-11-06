35f67cc57e7c3b94b082e332a5bbfbf51369be36
Thanathonster <73691152+Thanathonster@users.noreply.github.com>
2020-11-06 18:32:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/c
+ss/bootstrap.min.css" integrity="sha384-
+TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="a
+nonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" />
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !
+                        <p>
+                            <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" />
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !
+                        <p>
+                            <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" />
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !
+                        <p>
+                            <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
